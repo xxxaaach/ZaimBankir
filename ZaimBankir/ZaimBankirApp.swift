d3e431c6e13a58da51ec529390a8d8058a6c0574
xxxaaach <portnov.wadim@yandex.ru>
2024-12-09 17:25:50 +0300

    Initial Commit

---
+++ ZaimBankir/ZaimBankirApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaimBankirApp.swift
+//  ZaimBankir
+//
+//  Created by Вадим Портнов on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ZaimBankirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
